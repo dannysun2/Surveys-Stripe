460d99a8c131dd9866463a349958cc20185d173c
Danny Sun <danny.sun@me.com>
2019-04-06 01:46:53 -0500

    added client side

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 
 mongoose.connect(
   keys.mongoURI,
@@ -12,6 +13,8 @@ mongoose.connect(
 );
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
